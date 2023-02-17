753079ff68818cf8dddf5ce38c7bf70b3259d702
Edidiong Offong <donoffonglet_icy@yahoo.co.uk>
2023-02-17 03:34:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints alphabets in lower case
+*Return: Always 0
+*/
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar('\n');
+	return (0);
+}
