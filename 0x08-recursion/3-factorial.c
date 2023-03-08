55e52f954fa4ea3e6f761f92396cd9722af196fd
Edidiong Offong <donoffonglet_icy@yahoo.co.uk>
2023-03-08 02:42:44 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*factorial - find factorial of n
+*@n: number
+*Return: factorial of number, -1 if negative n
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
